d8b1f310dc9b3f7dcb97f10ce228dd2ffe2a1126
Abhinay Agarwal <rares.deoanca21@gmail.com>
2021-04-15 19:23:13 +0300

    Commit Necesar
    .

---
+++ Assets/Main.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        // aici vom scrie cod  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
